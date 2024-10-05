2f9631b476159d35c357d517803cdd14bc73b020
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-05 19:43:14 +0300

    feat: Add async new method to Table trait for improved functionality

--- src/ports/output/database/table.rs
+++ src/ports/output/database/table.rs
@@ -4,12 +4,13 @@ use crate::domain::types::Error;
 
 type Result<T> = std::result::Result<T, Error>;
 
-pub trait Table {
+pub trait Table: Sized {
     type Item: Clone + Hash + PartialEq;
     type Id: Clone + Hash + PartialEq;
 
     const NAME: &'static str;
 
+    async fn new<T>(args: T) -> Result<Self>;
     async fn create(&self, item: &Self::Item) -> Result<Self::Id>;
     async fn read(&self, id: &Self::Id) -> Option<Self::Item>;
     async fn update(&self, item: &Self::Item) -> Result<Self::Id>;
