eb975839d593c55e90c74af661c538466b347170
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-05 19:40:46 +0300

    feat: Add async table method to Database trait in database module

--- src/ports/output/database/mod.rs
+++ src/ports/output/database/mod.rs
@@ -1,9 +1,11 @@
-pub mod table;
+mod table;
 
 pub use crate::domain::types::error::DatabaseError;
+pub use table::Table;
 
 type Result<T> = std::result::Result<T, DatabaseError>;
 
 pub trait Database: Sized {
     async fn new<T>(args: T) -> Result<Self>;
+    async fn table<'a, T: Table + 'a>(&'a self) -> Result<&'a T>;
 }
