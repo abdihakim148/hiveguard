a2c90c8c1b6b1e068b0e99d5d17e30175e73d2fa
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-05 17:17:07 +0300

    feat: use a generic type for ID in the Table trait instead of &str

--- src/ports/output/database/table.rs
+++ src/ports/output/database/table.rs
@@ -1,8 +1,9 @@
 pub trait Table {
     type Item;
+    type Id;
 
     fn create(&self, item: &Self::Item) -> Result<(), String>;
-    fn read(&self, id: &str) -> Option<Self::Item>;
+    fn read(&self, id: &Self::Id) -> Option<Self::Item>;
     fn update(&self, item: &Self::Item) -> Result<(), String>;
-    fn delete(&self, id: &str) -> Result<(), String>;
+    fn delete(&self, id: &Self::Id) -> Result<(), String>;
 }
