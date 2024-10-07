21b67e579e543f8074b68398436f2dc62f4771dc
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-07 20:43:18 +0300

    feat: Implement From traits for Number enum using a macro

--- src/domain/types/number.rs
+++ src/domain/types/number.rs
@@ -1,4 +1,41 @@
 use std::convert::TryFrom;
+
+macro_rules! impl_from_for_number {
+    ($($t:ty => $variant:ident),*) => {
+        $(
+            impl From<$t> for Number {
+                fn from(value: $t) -> Self {
+                    Number::$variant(value)
+                }
+            }
+
+            impl From<Number> for $t {
+                fn from(number: Number) -> Self {
+                    if let Number::$variant(value) = number {
+                        value
+                    } else {
+                        panic!("Invalid conversion from Number to {}", stringify!($t));
+                    }
+                }
+            }
+        )*
+    };
+}
+
+impl_from_for_number!(
+    u8 => U8,
+    i8 => I8,
+    u16 => U16,
+    i16 => I16,
+    u32 => U32,
+    i32 => I32,
+    u64 => U64,
+    i64 => I64,
+    u128 => U128,
+    i128 => I128,
+    f32 => F32,
+    f64 => F64
+);
 use serde::{Serialize, Deserialize};
 
 /// Enum representing various numerical types.
