ccff88411a69f68322fbf5d6dd69682724b78d40
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-07 20:55:13 +0300

    feat: Create Value enum to represent various object types

---
+++ src/domain/types/value.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+use super::number::Number;
+
+/// Enum representing various possible object types.
+#[derive(Clone, Debug, PartialEq)]
+pub enum Value {
+    None,
+    Bool(bool),
+    Number(Number),
+    String(String),
+    Object(HashMap<String, Value>),
+    Vec(Vec<Value>),
+    // Add other variants as needed
+}
