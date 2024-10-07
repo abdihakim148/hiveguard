598fbeed6b9e1608ee89b6b4b091f714df185e07
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-07 21:02:23 +0300

    feat: Implement From and TryFrom traits for Value enum and derive Serialize/Deserialize

--- src/domain/types/value.rs
+++ src/domain/types/value.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
+use std::convert::{TryFrom, TryInto};
+use serde::{Serialize, Deserialize};
 use super::number::Number;
 
 /// Enum representing various possible object types.
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum Value {
     None,
     Bool(bool),
@@ -10,4 +12,111 @@ pub enum Value {
     String(String),
     Object(HashMap<String, Value>),
     Vec(Vec<Value>),
+}
+impl From<()> for Value {
+    fn from(_: ()) -> Self {
+        Value::None
+    }
+}
+
+impl From<bool> for Value {
+    fn from(value: bool) -> Self {
+        Value::Bool(value)
+    }
+}
+
+impl<T: Into<Number>> From<T> for Value {
+    fn from(value: T) -> Self {
+        Value::Number(value.into())
+    }
+}
+
+impl From<String> for Value {
+    fn from(value: String) -> Self {
+        Value::String(value)
+    }
+}
+
+impl From<&str> for Value {
+    fn from(value: &str) -> Self {
+        Value::String(value.to_string())
+    }
+}
+
+impl From<HashMap<String, Value>> for Value {
+    fn from(value: HashMap<String, Value>) -> Self {
+        Value::Object(value)
+    }
+}
+
+impl From<Vec<Value>> for Value {
+    fn from(value: Vec<Value>) -> Self {
+        Value::Vec(value)
+    }
+}
+
+impl TryFrom<Value> for () {
+    type Error = &'static str;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::None => Ok(()),
+            _ => Err("Invalid conversion to ()"),
+        }
+    }
+}
+
+impl TryFrom<Value> for bool {
+    type Error = &'static str;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Bool(b) => Ok(b),
+            _ => Err("Invalid conversion to bool"),
+        }
+    }
+}
+
+impl TryFrom<Value> for String {
+    type Error = &'static str;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::String(s) => Ok(s),
+            _ => Err("Invalid conversion to String"),
+        }
+    }
+}
+
+impl TryFrom<Value> for HashMap<String, Value> {
+    type Error = &'static str;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Object(o) => Ok(o),
+            _ => Err("Invalid conversion to HashMap<String, Value>"),
+        }
+    }
+}
+
+impl TryFrom<Value> for Vec<Value> {
+    type Error = &'static str;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Vec(v) => Ok(v),
+            _ => Err("Invalid conversion to Vec<Value>"),
+        }
+    }
+}
+
+impl<T: TryFrom<Number>> TryFrom<Value> for T {
+    type Error = &'static str;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Number(n) => n.try_into(),
+            _ => Err("Invalid conversion from Value"),
+        }
+    }
 }
