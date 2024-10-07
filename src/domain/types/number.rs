cfacd2c581aeb8f0a58686e67b84bf71c58b59a8
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-07 20:33:31 +0300

    feat: Add Number enum to represent various numerical types with traits

---
+++ src/domain/types/number.rs
@@ -0,0 +1,18 @@
+use serde::{Serialize, Deserialize};
+
+/// Enum representing various numerical types.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Number {
+    U8(u8),
+    I8(i8),
+    U16(u16),
+    I16(i16),
+    U32(u32),
+    I32(i32),
+    U64(u64),
+    I64(i64),
+    U128(u128),
+    I128(i128),
+    F32(f32),
+    F64(f64),
+}
