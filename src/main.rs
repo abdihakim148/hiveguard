cb628071e7feece98654c64876d37e36d9157d9f
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-13 14:25:46 +0300

    start the actix server

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ mod domain;
 /// Module for input and output ports.
 mod ports;
 
+
+use adaptors::inputs::api::actix::Actix;
+
 /// Entry point of the application.
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    Actix::start().await
 }
