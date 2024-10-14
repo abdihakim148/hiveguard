05a6add703fe7a8c8d7c304011de51d6708da381
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-14 15:22:42 +0300

    feat: Add Mailer trait for flexible email sending implementation

---
+++ src/ports/outputs/mailer.rs
@@ -0,0 +1,26 @@
+pub trait Mailer: Sized {
+    type Config;
+    type Mail;
+
+    /// Creates a new instance of the mailer with the given configuration.
+    ///
+    /// # Arguments
+    ///
+    /// * `config` - Configuration settings for the mailer.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<Self>` - Returns a new instance of the mailer wrapped in a `Result`.
+    async fn new(config: Self::Config) -> Result<Self>;
+
+    /// Sends an email.
+    ///
+    /// # Arguments
+    ///
+    /// * `mail` - The mail details to be sent.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<()>` - Returns `Ok(())` if the email is sent successfully, or an error otherwise.
+    async fn send(&self, mail: Self::Mail) -> Result<()>;
+}
