2adb239663919116161395e75112754f6a099ab2
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-17 14:27:18 +0300

    changed the error trait, deleted the database_error module and added features.

--- src/ports/traits/error.rs
+++ src/ports/traits/error.rs
@@ -1,25 +1,11 @@
 use std::error::Error as StdError;
-use std::fmt::Display;
-use actix_web::http::StatusCode;
-#[cfg(feature = "actix")]
-use actix_web::{error::ResponseError, http::StatusCode, HttpResponse as Response, body::BoxBody, ResponseError};
-
-
-pub trait Error: StdError + Display {
-    #[cfg(feature = "actix")]
-    fn status(&self) -> StatusCode;
-    #[cfg(feature = "actix")]
-    fn reponse(&self) -> Response<BoxBody>;
-}
 
 
 #[cfg(feature = "actix")]
-impl<T: Error> ResponseError for T {
-    fn status_code(&self) -> StatusCode {
-        self.status()
-    }
+use actix_web::ResponseError;
 
-    fn error_response(&self) -> Response<BoxBody> {
-        self.response()
-    }
-}
+#[cfg(not(feature = "actix"))]
+pub trait Error: StdError {}
+
+#[cfg(feature = "actix")]
+pub trait Error: StdError + ResponseError {}
