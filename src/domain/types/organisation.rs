6bd2a536ba402c58074524eb871c8c300dbbcf1a
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-10-24 17:46:08 +0300

    feat: Create Organisation struct with fields for id, emails, domain, home, and phone

---
+++ src/domain/types/organisation.rs
@@ -0,0 +1,20 @@
+use bson::oid::ObjectId;
+use serde::{Serialize, Deserialize};
+use super::EmailAddress;
+
+/// A struct representing an organisation.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Organisation {
+    /// The unique identifier for the organisation.
+    pub id: ObjectId,
+    /// The name of the organisation.
+    pub name: String,
+    /// A list of named email addresses associated with the organisation.
+    pub emails: Vec<(String, EmailAddress)>,
+    /// The domain of the organisation, if available.
+    pub domain: Option<String>,
+    /// The home URL of the organisation, if available.
+    pub home: Option<String>,
+    /// A list of named phone numbers associated with the organisation.
+    pub phone: Vec<(String, String)>,
+}
