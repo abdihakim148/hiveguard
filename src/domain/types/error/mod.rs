19722d329e4324b9af0287362f1ac1d627412334
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-17 18:11:00 +0300

    update the Error enum by adding more variants and implementations for easier conversion from the PasetoError

--- src/domain/types/error/mod.rs
+++ src/domain/types/error/mod.rs
@@ -1,18 +1,19 @@
 #![allow(unused)]
 mod conversion;
 
-use std::fmt::{self, Debug as DebugTrait, Display, Result};
-use thiserror::Error as ThisError;
-use std::error::Error as StdError;
-use crate::ports::Error as GlobalError;
-use argon2::password_hash::errors::Error as HashError;
-use serde_json::Error as JsonError;
-pub use conversion::ConversionError;
-use lettre::address::AddressError as EmailAddressError;
-pub use super::r#type::*;
-use super::{Value, Number};
 #[cfg(feature = "actix")]
 use actix_web::{error::ResponseError, http::StatusCode, HttpResponse as Response, body::BoxBody, HttpResponseBuilder as ResponseBuilder};
+use std::fmt::{self, Debug as DebugTrait, Display, Result};
+use lettre::address::AddressError as EmailAddressError;
+use argon2::password_hash::errors::Error as HashError;
+use crate::ports::Error as GlobalError;
+pub use conversion::ConversionError;
+use rusty_paseto::core::PasetoError;
+use serde_json::Error as JsonError;
+use thiserror::Error as ThisError;
+use std::error::Error as StdError;
+use super::{Value, Number};
+pub use super::r#type::*;
 
 #[derive(Debug, ThisError)]
 pub enum Error<T: DebugTrait = Value> {
@@ -22,6 +23,12 @@ pub enum Error<T: DebugTrait = Value> {
     HashingError(HashError),
     #[error("domain: email_error: {0}")]
     EmailAddressError(EmailAddressError),
+    #[error("invalid token")]
+    InvalidToken,
+    #[error("paseto error: {0}")]
+    PasetoError(PasetoError),
+    #[error("internal server error: {0}")]
+    InternalServerError(Box<dyn std::error::Error>),
     #[error("{0}")]
     New(GlobalError)
 }
@@ -57,9 +64,22 @@ impl From<Error<Number>> for Error<Value> {
     fn from(err: Error<Number>) -> Self {
         match err {
             Error::ConversionError(err) => Error::ConversionError(ConversionError{expected: err.expected, found: err.found, value: Value::Number(err.value)}),
-            Error::HashingError(err) => Error::HashingError(err),
             Error::EmailAddressError(err) => Error::EmailAddressError(err),
+            Error::HashingError(err) => Error::HashingError(err),
+            Error::InvalidToken => Error::InvalidToken,
+            Error::PasetoError(err) => Error::PasetoError(err),
+            Error::InternalServerError(err) => Error::InternalServerError(err),
             Error::New(err) => Error::New(err)
+        }
+    }
+}
+
+
+impl From<PasetoError> for Error {
+    fn from(err: PasetoError) -> Self {
+        match err {
+            PasetoError::InvalidSignature => Error::InvalidToken,
+            _ => Error::PasetoError(err),
         }
     }
 }
@@ -70,7 +90,8 @@ impl<T: DebugTrait> ResponseError for Error<T> {
     fn status_code(&self) -> StatusCode {
         match self {
             Self::ConversionError(_) | Self::EmailAddressError(_) => StatusCode::BAD_REQUEST,
-            Self::HashingError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::HashingError(_) | Self::PasetoError(_) | Self::InternalServerError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::InvalidToken => StatusCode::UNAUTHORIZED,
             Self::New(err) => err.status_code() 
         }
     }
@@ -81,7 +102,8 @@ impl<T: DebugTrait> ResponseError for Error<T> {
         builder.content_type("application/json");
         let body = match self {
             Self::ConversionError(_) | Self::EmailAddressError(_) => BoxBody::new(format!("{{\"error\": \"{self}\"}}")),
-            Self::HashingError(_) => BoxBody::new(format!("{{\"error\": \"Internal Server Error\"}}")),
+            Self::HashingError(_) | Self::PasetoError(_) | Self::InternalServerError(_) => BoxBody::new(format!("{{\"error\": \"Internal Server Error\"}}")),
+            Self::InvalidToken => BoxBody::new(format!("invalid token")),
             Self::New(err) => err.error_response().into_body()
         };
         builder.body(body)
