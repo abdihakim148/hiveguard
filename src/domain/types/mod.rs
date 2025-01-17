61bc527f871d16f514e145bb8c46ab4f27c9e084
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-17 09:00:01 +0300

    created the either moule with the Either enum

--- src/domain/types/mod.rs
+++ src/domain/types/mod.rs
@@ -1,24 +1,24 @@
-/// Module for user-related types.
-mod user;
-/// Module for error-related types.
-mod error;
-mod number;
-mod value;
-mod email;
+mod organisation;
 mod permission;
 mod resource;
-mod role;
-mod organisation;
+mod number;
+mod either;
 mod member;
 mod r#type;
 mod config;
+mod error;
+mod value;
+mod email;
+mod user;
+mod role;
 
 /// Re-exporting types for external access.
-pub use error::*;
-pub use user::*;
-pub use value::*;
-pub use email::*;
-pub use resource::*;
 pub use permission::*;
+pub use resource::*;
+pub use either::*;
 pub use number::*;
 pub use config::*;
+pub use error::*;
+pub use value::*;
+pub use email::*;
+pub use user::*;
