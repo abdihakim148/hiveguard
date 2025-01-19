81819f5fc36055869609bd42be69c06b5258f45b
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-19 18:04:28 +0300

    feat: Add Secret trait for environment variable management in config module

--- src/domain/types/config/mod.rs
+++ src/domain/types/config/mod.rs
@@ -1,7 +1,40 @@
 mod password;
 mod argon;
 
-
+use std::error::Error as StdError;
+use std::env::var;
 use super::*;
 
 pub use password::*;
+
+#[allow(unused)]
+pub trait Secret: Sized {
+    type Error;
+    const CHAR: char = '$';
+    /// This function is used to retrieve a secret from an env variable.
+    fn single(key: &str) -> Result<Self, Self::Error>;
+    /// This function will be used to retrieve a secret from a place more secure than environment variables.
+    fn double(key: &str) -> Result<Self, Self::Error>;
+    /// This function will be used to retrieve a secret from a place a maximum security.
+    fn tripple(key: &str) -> Result<Self, Self::Error>;
+}
+
+
+impl<T> Secret for T 
+where
+     T: TryFrom<String>,
+     T::Error: StdError + 'static,
+{
+    type Error = Box<dyn StdError + 'static>;
+    fn single(key: &str) -> Result<Self, Self::Error> {
+        Ok(var(key)?.try_into()?)
+    }
+
+    fn double(_key: &str) -> Result<Self, Self::Error> {
+        Err("")?
+    }
+
+    fn tripple(_key: &str) -> Result<Self, Self::Error> {
+        Err("")?
+    }
+}
