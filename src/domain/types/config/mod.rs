16a2a282f32d05db2e9fcafb1bf74b9e063df464
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-20 19:32:00 +0300

    add a process method to combine the methods and provide out of the box solution

--- src/domain/types/config/mod.rs
+++ src/domain/types/config/mod.rs
@@ -10,13 +10,33 @@ pub use password::*;
 #[allow(unused)]
 pub trait Secret: Sized {
     type Error;
-    const CHAR: char = '$';
+    const DELIMETER: u8 = b'$';
+    fn process(key: &str) -> Result<Option<Self>, <Self as Secret>::Error> {
+        let mut bytes = match key.as_bytes().get(0..3){
+            Some(elements) => elements,
+            None => key.as_bytes()
+        }.into_iter().peekable();
+        let mut round = 0u8;
+        while let Some(byte) = bytes.peek() {
+            if *byte == &Self::DELIMETER {
+                bytes.next();
+                round+=1;
+            }
+        }
+        match round {
+            0 => Ok(None),
+            1 => Ok(Some(Self::single(key)?)),
+            2 => Ok(Some(Self::double(key)?)),
+            3 => Ok(Some(Self::tripple(key)?)),
+            _ => Ok(None)
+        }
+    }
     /// This function is used to retrieve a secret from an env variable.
-    fn single(key: &str) -> Result<Self, Self::Error>;
+    fn single(key: &str) -> Result<Self, <Self as Secret>::Error>;
     /// This function will be used to retrieve a secret from a place more secure than environment variables.
-    fn double(key: &str) -> Result<Self, Self::Error>;
+    fn double(key: &str) -> Result<Self, <Self as Secret>::Error>;
     /// This function will be used to retrieve a secret from a place a maximum security.
-    fn tripple(key: &str) -> Result<Self, Self::Error>;
+    fn tripple(key: &str) -> Result<Self, <Self as Secret>::Error>;
 }
 
 
@@ -26,15 +46,15 @@ where
      T::Error: StdError + 'static,
 {
     type Error = Box<dyn StdError + 'static>;
-    fn single(key: &str) -> Result<Self, Self::Error> {
+    fn single(key: &str) -> Result<Self, <Self as Secret>::Error> {
         Ok(var(key)?.try_into()?)
     }
 
-    fn double(_key: &str) -> Result<Self, Self::Error> {
+    fn double(_key: &str) -> Result<Self, <Self as Secret>::Error> {
         Err("")?
     }
 
-    fn tripple(_key: &str) -> Result<Self, Self::Error> {
+    fn tripple(_key: &str) -> Result<Self, <Self as Secret>::Error> {
         Err("")?
     }
 }
