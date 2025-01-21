fa1ea2dbf55fa53eff705c38578d55efb46d4476
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-21 17:50:30 +0300

    updated the Config trait

--- src/ports/inputs/config.rs
+++ src/ports/inputs/config.rs
@@ -2,30 +2,33 @@
  * Trait for configuration management, providing methods to load and save configurations.
  *
  * This trait defines the necessary operations for handling configuration data,
- * including loading from and saving to a specified path.
+ * including loading from and saving to a specified path. It uses an associated type
+ * for configuration and input, allowing flexibility in how configurations are managed.
  */
 pub trait Config: Sized {
     type Error;
+    type Config;
+    type Input;
     /// This would be the default path for the configuration file.
-    const PATH: &'static str = "./config.yaml";
-    /// Loads the configuration from a specified file path.
+    const PATH: &'static str = "config.yaml";
+    /// Loads the configuration from a specified input source.
     ///
     /// # Arguments
     ///
-    /// * `path` - An optional string slice representing the path to load the configuration from.
+    /// * `input` - An optional input source for loading the configuration.
     ///
     /// # Returns
     ///
-    /// * `Result<Self, Self::Error>` - The loaded configuration wrapped in a `Result`.
-    async fn load(path: Option<&str>) -> Result<Self, Self::Error>;
-    /// Saves the configuration to a specified file path.
+    /// * `Result<Self::Config, Self::Error>` - The loaded configuration wrapped in a `Result`.
+    async fn load(input: Option<Self::Input>) -> Result<Self::Config, Self::Error>;
+    /// Saves the configuration to a specified input source.
     ///
     /// # Arguments
     ///
-    /// * `path` - An optional string slice representing the path to save the configuration to.
+    /// * `input` - An optional input source for saving the configuration.
     ///
     /// # Returns
     ///
-    /// * `Result<Self, Self::Error>` - The result of the save operation wrapped in a `Result`.
-    async fn save(path: Option<&str>) -> Result<Self, Self::Error>;
+    /// * `Result<Self::Config, Self::Error>` - The result of the save operation wrapped in a `Result`.
+    async fn save(input: Option<Self::Input>) -> Result<Self::Config, Self::Error>;
 }
