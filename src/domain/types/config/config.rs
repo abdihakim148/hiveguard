a68ce76a08970375f9c8f2475f1000e49db0343c
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-21 20:51:56 +0300

    add methods to expose the inner variables as reference

--- src/domain/types/config/config.rs
+++ src/domain/types/config/config.rs
@@ -4,9 +4,25 @@ use super::{argon::Argon, Paseto};
 
 
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
 pub struct Config<DB: Database> {
     database: DB,
     argon: Argon,
     paseto: Paseto
+}
+
+
+
+impl<DB: Database> Config<DB> {
+    pub fn db(&self) -> &DB {
+        &self.database
+    }
+
+    pub fn argon(&self) -> &Argon {
+        &self.argon
+    }
+
+    pub fn paseto(&self) -> &Paseto {
+        &self.paseto
+    }
 }
