630ad101fb9171ed03224d7a94cac624ad34d072
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-24 21:28:12 +0300

    implement Serialize and Deserialize traits which should never be used. it is just a dummy implementation

--- src/adaptors/outputs/mailer/smtp.rs
+++ src/adaptors/outputs/mailer/smtp.rs
@@ -1,6 +1,7 @@
 use lettre::{message::{Mailbox, Message, SinglePart}, transport::smtp::PoolConfig, AsyncSmtpTransport, Tokio1Executor, AsyncTransport};
 use crate::ports::outputs::mailer::Mailer;
 use crate::domain::types::{Error, Value};
+use serde::{Serialize, Deserialize};
 use crate::domain::types::Mail;
 use crate::ports::Result;
 
@@ -49,5 +50,27 @@ impl TryFrom<Mail> for SmtpMailer {
         }
         let client = mailer.pool_config(PoolConfig::new()).build();
         Ok(SmtpMailer(client))
+    }
+}
+
+/// This implementation should never be used.
+/// It is implemented just to satisfy a trait requirement
+impl Serialize for SmtpMailer {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer {
+        serializer.serialize_str("")
+    }
+}
+
+
+/// This implementation should never be used.
+/// It is implemented just to satisfy a trait requirement
+/// This will always result in an error.
+impl<'de> Deserialize<'de> for SmtpMailer {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de> {
+        Err(serde::de::Error::custom("This operation should never be used"))
     }
 }
