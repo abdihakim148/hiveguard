66839686c220cb0ec34fbd81f38f03e08fbb52ef
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-25 17:48:41 +0300

    reduce the reliance of async code to avoid the use of another runtime inside a sync func that which depends on an async func

--- src/domain/types/config/config.rs
+++ src/domain/types/config/config.rs
@@ -8,9 +8,9 @@ use std::io::{Read, Write};
 
 
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
 pub struct Config<DB: Database + Default, M: Mailer + TryFrom<Mail>> 
-where M::Error: std::fmt::Display
+where M::Error: std::fmt::Display + std::fmt::Debug,
 {
     database: DB,
     argon: Argon,
@@ -20,8 +20,10 @@ where M::Error: std::fmt::Display
 
 
 
-impl<DB: Database + Default, M: Mailer + TryFrom<Mail>> Config<DB, M> 
-where M::Error: std::fmt::Display
+impl<DB: Database + Default + Serialize + DeserializeOwned, M: Mailer + TryFrom<Mail> + Default> Config<DB, M> 
+where 
+    M: Mailer + TryFrom<Mail> + Serialize + DeserializeOwned,
+    M::Error: std::fmt::Display + std::fmt::Debug,
 {
     pub fn db(&self) -> &DB {
         &self.database
@@ -38,20 +40,9 @@ where M::Error: std::fmt::Display
     pub fn mailer(&self) -> &MailConfig<M> {
         &self.mailer
     }
-}
 
-
-impl<DB: Database + Default + Serialize + DeserializeOwned, M> ConfigTrait for Config<DB, M> 
-where 
-    M: Mailer + TryFrom<Mail> + Serialize + DeserializeOwned,
-    M::Error: std::fmt::Display,
-{
-    type Error = Box<dyn std::error::Error + 'static>;
-    type Input = ();
-    
-
-    async  fn load(path: Option<&str>, input: Self::Input) -> Result<Self, Self::Error> {
-        let path = match path {Some(path) => path, None => Self::PATH};
+    fn load_sync(path: Option<&str>, input: <Self as ConfigTrait>::Input) -> Result<Self, <Self as ConfigTrait>::Error> {
+        let path = match path {Some(path) => path, None => <Self as ConfigTrait>::PATH};
         match std::fs::File::open(path) {
             Ok(mut file) => {
                 let mut buf = String::new();
@@ -61,13 +52,13 @@ where
             _ => {
                 let config = <Self as Default>::default();
                 let path = Some(path);
-                config.save(path, input).await?;
+                config.save_sync(path, input)?;
                 Ok(config)
             }
         }
     }
 
-    async fn save(&self, path: Option<&str>, _input: Self::Input) -> Result<(), Self::Error> {
+    fn save_sync(&self, path: Option<&str>, _input: <Self as ConfigTrait>::Input) -> Result<(), <Self as ConfigTrait>::Error> {
         let path = match path {Some(path) => path, None => Self::PATH};
         let json = serde_json::to_string(&self)?;
         let mut file = std::fs::OpenOptions::new().write(true).create(true).open(path)?;
@@ -78,15 +69,20 @@ where
 }
 
 
-/// This implementations panics.
-impl<DB: Database + Default + Serialize + DeserializeOwned, M> Default for Config<DB, M> 
+impl<DB: Database + Default + Serialize + DeserializeOwned, M> ConfigTrait for Config<DB, M> 
 where 
-    M: Mailer + TryFrom<Mail> + Serialize + DeserializeOwned,
-    M::Error: std::fmt::Display,
+    M: Mailer + TryFrom<Mail> + Serialize + DeserializeOwned + Default,
+    M::Error: std::fmt::Display + std::fmt::Debug,
 {
-    fn default() -> Self {
-        let runtime = tokio::runtime::Runtime::new().unwrap();
-        let config = runtime.block_on(Config::load(None, ())).unwrap();
-        config
+    type Error = Box<dyn std::error::Error + 'static>;
+    type Input = ();
+    
+
+    async  fn load(path: Option<&str>, input: Self::Input) -> Result<Self, Self::Error> {
+        Self::load_sync(path, input)
+    }
+
+    async fn save(&self, path: Option<&str>, input: Self::Input) -> Result<(), Self::Error> {
+        self.save_sync(path, input)
     }
 }
