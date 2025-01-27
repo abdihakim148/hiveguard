efa65bd3f881429ca1f755dbca8d8126d977e9c2
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-27 14:59:26 +0300

    remove the Default implementation of the SmtpMailer

--- src/adaptors/outputs/mailer/smtp.rs
+++ src/adaptors/outputs/mailer/smtp.rs
@@ -74,12 +74,3 @@ impl<'de> Deserialize<'de> for SmtpMailer {
         Err(serde::de::Error::custom("This operation should never be used"))
     }
 }
-
-
-
-impl Default for SmtpMailer {
-    fn default() -> Self {
-        let client = Client::unencrypted_localhost();
-        SmtpMailer(client)
-    }
-}
