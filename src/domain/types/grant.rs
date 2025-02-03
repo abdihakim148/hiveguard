a5963501456c7cac286e24a3f0d43814c809ef66
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-02-03 15:23:02 +0300

    test the Serialization and Deserialization logic

--- src/domain/types/grant.rs
+++ src/domain/types/grant.rs
@@ -6,14 +6,14 @@ use std::fmt;
 use std::str::FromStr;
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Grant(pub (ObjectId, Permission));
+pub struct Grant(pub ObjectId, pub Permission);
 
 impl Serialize for Grant {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let (object_id, permission) = &self.0;
+        let (object_id, permission) = (&self.0, &self.1);
         let s = format!("{}:{}", object_id.to_hex(), permission);
         serializer.serialize_str(&s)
     }
@@ -43,10 +43,39 @@ impl<'de> Deserialize<'de> for Grant {
                 }
                 let object_id = ObjectId::from_str(parts[0]).map_err(de::Error::custom)?;
                 let permission = parts[1].parse().map_err(de::Error::custom)?;
-                Ok(Grant((object_id, permission)))
+                Ok(Grant(object_id, permission))
             }
         }
 
         deserializer.deserialize_str(GrantVisitor)
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use serde_json::{to_string, from_str};
+    use super::{Grant, super::Permission};
+    use bson::oid::ObjectId;
+
+
+    #[test]
+    fn test_serialization() {
+        let id = ObjectId::default();
+        let id_str = id.to_hex();
+        let permission = Permission::Delete;
+        let grant = Grant(id, permission);
+        let json = to_string(&grant).unwrap();
+        assert_eq!(format!("\"{id_str}:delete\""), json)
+    }
+
+    #[test]
+    fn test_deserialization() {
+        let json = "\"000000000000000000000000:update\"";
+        let grant = from_str::<Grant>(json).unwrap();
+        let id = ObjectId::from_bytes([0u8; 12]);
+        let permission = Permission::Update;
+        assert_eq!(Grant(id, permission), grant);
     }
 }
