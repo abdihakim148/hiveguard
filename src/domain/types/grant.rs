ace364025edba31725338dcea12b55f51d591d0a
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-02-03 15:02:56 +0300

    created the Grant data type and implemented serialization and Deserialization

---
+++ src/domain/types/grant.rs
@@ -0,0 +1,52 @@
+use bson::oid::ObjectId;
+use super::Permission;
+use serde::{Serialize, Deserialize, Serializer, Deserializer};
+use serde::de::{self, Visitor};
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Grant(pub (ObjectId, Permission));
+
+impl Serialize for Grant {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let (object_id, permission) = &self.0;
+        let s = format!("{}:{}", object_id.to_hex(), permission);
+        serializer.serialize_str(&s)
+    }
+}
+
+impl<'de> Deserialize<'de> for Grant {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct GrantVisitor;
+
+        impl<'de> Visitor<'de> for GrantVisitor {
+            type Value = Grant;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a string in the format 'rource_id:permission'")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Grant, E>
+            where
+                E: de::Error,
+            {
+                let parts: Vec<&str> = value.split(':').collect();
+                if parts.len() != 2 {
+                    return Err(de::Error::invalid_value(de::Unexpected::Str(value), &self));
+                }
+                let object_id = ObjectId::from_str(parts[0]).map_err(de::Error::custom)?;
+                let permission = parts[1].parse().map_err(de::Error::custom)?;
+                Ok(Grant((object_id, permission)))
+            }
+        }
+
+        deserializer.deserialize_str(GrantVisitor)
+    }
+}
