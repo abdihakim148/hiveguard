919cb67a1769817a8b2ffd28d4298fb7e925bac4
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-07-02 23:14:05 +0300

    update the authentication's domain logic

--- hiveguard/src/domain/authentication.rs
+++ hiveguard/src/domain/authentication.rs
@@ -1,5 +1,5 @@
-use crate::ports::outputs::database::Database;
-use crate::types::{Error, User, TokenBundle, Email, DatabaseError};
+use crate::ports::outputs::database::{Database, tables::{UsersTable, VerificationsTable, SessionsTable}};
+use crate::types::{Error, User, TokenBundle, Email, DatabaseError, Verification, Id, Session};
 use super::{Password, Tokenizer};
 
 
@@ -7,29 +7,33 @@ pub struct Authentication;
 
 
 impl Authentication {
-    pub async fn signup<DB: Database, T: Tokenizer, Hasher: Password>(db: &DB, mut user: User, tokenizer: &T, hasher: Hasher) -> Result<TokenBundle, Error>
+    pub async fn signup<DB: Database<UsersTable: UsersTable<DB::Client, Item = User>, VerificationsTable: VerificationsTable<DB::Client, Item = Verification<Id>>, SessionsTable: SessionsTable<DB::Client, Item = Session>>, T: Tokenizer, Hasher: Password>(db: &DB, mut user: User, tokenizer: &T, hasher: Hasher) -> Result<TokenBundle, Error>
     where
         Error: From<DB::Error>,
+        Error: From<T::Error>,
+        T::Error: From<DB::Error>
     {
-        let password = user.login.password().ok_or(Error::InvalidCredentials)?;
+        let password = user.login.password()?;
         let hash = hasher.hash_password(password)?;
         user.login.set_hash(hash);
         let subject = user.id;
         db.create_user(user).await?;
-        tokenizer.generate_token(db, subject).await
+        Ok(tokenizer.generate_token(db, subject).await?)
     }
 
-    pub async fn login<DB: Database, T: Tokenizer, Verifyer: Password>(db: &DB, email: Email, password: String, tokenizer: &T, verifyer: Verifyer) -> Result<TokenBundle, Error> 
+    pub async fn login<DB: Database<UsersTable: UsersTable<DB::Client, Item = User>, VerificationsTable: VerificationsTable<DB::Client, Item = Verification<Id>>, SessionsTable: SessionsTable<DB::Client, Item = Session>>, T: Tokenizer, Verifyer: Password>(db: &DB, email: Email, password: String, tokenizer: &T, verifyer: Verifyer) -> Result<TokenBundle, Error> 
     where
         Error: From<DB::Error>,
+        Error: From<T::Error>,
+        T::Error: From<DB::Error>
     {
         let user = match db.get_user_by_email(email).await?{
             Some(user) => user,
             None => return Err(Error::DatabaseError(DatabaseError::UserNotFound)),
         };
-        let hash = user.login.password().ok_or(Error::InvalidCredentials)?;
+        let hash = user.login.password()?;
         verifyer.verify_password(&password, hash)?;
         let subject = user.id;
-        tokenizer.generate_token(db, subject).await
+        Ok(tokenizer.generate_token(db, subject).await?)
     }
 }
--- hiveguard/src/domain/tokenization/mod.rs
+++ hiveguard/src/domain/tokenization/mod.rs
@@ -1,11 +1,12 @@
-use crate::types::{Error, Token, TokenBundle, Id};
-use crate::ports::outputs::database::Database;
+use crate::ports::outputs::database::{Database, tables::SessionsTable};
+use crate::types::{Token, TokenBundle, Id, Session};
 
 
 pub trait Tokenizer {
-    async fn generate_token<DB: Database>(&self, db: &DB, subject: Id) -> Result<TokenBundle, Error>;
-    async fn renew_token<DB: Database>(&self, db: &DB, token: &Token) -> Result<Token, Error>;
-    async fn renew_refresh_token<DB: Database>(&self, db: &DB, token: &Token) -> Result<Token, Error>;
-    async fn invalidate_token<DB: Database>(&self, db: &DB, token: &Token) -> Result<(), Error>;
-    async fn validate_token(&self, token: &Token) -> Result<(), Error>;
+    type Error;
+    async fn generate_token<DB: Database<SessionsTable: SessionsTable<DB::Client, Item = Session>>>(&self, db: &DB, subject: Id) -> Result<TokenBundle, Self::Error> where Self::Error: From<DB::Error>;
+    async fn renew_token<DB: Database<SessionsTable: SessionsTable<DB::Client, Item = Session>>>(&self, db: &DB, token: &Token) -> Result<Token, Self::Error> where Self::Error: From<DB::Error>;
+    async fn renew_refresh_token<DB: Database<SessionsTable: SessionsTable<DB::Client, Item = Session>>>(&self, db: &DB, token: &Token) -> Result<Token, Self::Error> where Self::Error: From<DB::Error>;
+    async fn invalidate_token<DB: Database<SessionsTable: SessionsTable<DB::Client, Item = Session>>>(&self, db: &DB, token: &Token) -> Result<(), Self::Error> where Self::Error: From<DB::Error>;
+    async fn validate_token(&self, token: &Token) -> Result<(), Self::Error>;
 }
